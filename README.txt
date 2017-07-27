24d75ba4a9f23cb60d2fdc664c574a2c7cb6ed12
Mike Hemberger <mike@thestizmedia.com>
2017-07-27 13:36:39 -0400

    Get it started.

---
+++ README.txt
@@ -0,0 +1,16 @@
+MAI LIFESTYLE PRO THEME
+https://maipro.io
+
+INSTALL
+1. Upload the Mai Lifestyle Pro theme folder via FTP to your wp-content/themes/ directory. (The Genesis parent theme needs to be in the wp-content/themes/ directory as well.)
+2. Go to your WordPress dashboard and select Appearance.
+3. Activate the Mai Lifestyle Pro theme.
+4. Inside your WordPress dashboard, go to Genesis > Theme Settings and configure them to your liking.
+
+SUPPORT
+Please visit http://support.maipro.io/ for theme support.
+
+CHANGELOG
+
+= 1.0.0 =
+* Initial release.
---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+
+// Child theme (Do not remove!)
+define( 'CHILD_THEME_NAME', 'Mai Lifestyle Pro' );
+define( 'CHILD_THEME_URL', 'https://maipro.io/' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+// Support the Mai Pro Engine (Do not remove!)
+add_theme_support( 'mai-pro-engine' );
+
+/**
+ * Mai Pro dependencies (Do not remove!).
+ * This is required for the theme to function properly.
+ */
+foreach ( glob( dirname( __FILE__ ) . '/includes/dependencies/*.php' ) as $file ) { include $file; }
+WP_Dependency_Installer::instance()->run( dirname( __FILE__ ) . '/includes/dependencies' );
+
+// Don't do anything else if the Mai Pro Engine plugin is not active.
+if ( ! class_exists( 'Mai_Pro_Engine' ) ) {
+	return;
+}
+
+// Include all php files in the /includes/ directory.
+foreach ( glob( dirname( __FILE__ ) . '/includes/*.php' ) as $file ) { include $file; }
+
+
+/**********************************
+ * Add your customizations below! *
+ **********************************/
+
+
+// Enqueue CSS files
+add_action( 'wp_enqueue_scripts', 'maip_enqueue_fonts' );
+function maip_enqueue_fonts() {
+	wp_enqueue_style( 'maip-google-fonts', '//fonts.googleapis.com/css?family=Lato:400,400i,700|Open+Sans:400,400i,700', array(), CHILD_THEME_VERSION );
+}
+
+// Customize the site footer text
+add_filter( 'genesis_footer_creds_text', 'maip_site_footer_text' );
+function maip_site_footer_text( $text ) {
+	$url  = 'https://maipro.io/';
+	$name = 'Mai Pro';
+	return sprintf( 'Copyright &copy; %s <a href="%s" title="%s">%s</a> &middot; All Rights Reserved &middot; Powered by <a rel="nofollow" href="%s">%s</a>',
+		date('Y'),
+		get_bloginfo('url'),
+		get_bloginfo('name'),
+		get_bloginfo('name'),
+		$url,
+		$name
+	);
+}
