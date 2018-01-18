5d7f5427f010edd19d763b507fc219407f6756cf
Mike Hemberger <mike@thestizmedia.com>
2018-01-18 12:55:36 -0500

    SP constant

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 define( 'CHILD_THEME_NAME', 'Mai Lifestyle Pro' );
 define( 'CHILD_THEME_URL', 'https://maitheme.com/' );
 define( 'CHILD_THEME_VERSION', '1.1.0' );
+define( 'MAI_THEME_SP', true );
 
 // Support the Mai Theme Engine (Do not remove!).
 add_theme_support( 'mai-theme-engine' );
